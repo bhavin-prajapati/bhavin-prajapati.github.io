57be95d2a21c6449bc1d033ec1b894b157dbb250
Bhavin Prajapati <bprajapati@tribalscale.com>
2019-02-12 23:50:10 -0500

    Updates

--- sw.js
+++ sw.js
@@ -26,20 +26,20 @@ workbox.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-c4f7bfcac735b5d1d819.js"
+    "url": "webpack-runtime-7f9e43748c8fd9d74125.js"
   },
   {
     "url": "app.57a75a5b3df48d434e69.css"
   },
   {
-    "url": "app-43054ff1edfcb3712b3d.js"
+    "url": "app-6bba3c07e2d625d82329.js"
   },
   {
     "url": "component---node-modules-gatsby-plugin-offline-app-shell-js-5d72abde5d94b31a9396.js"
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "1599af76d61aa2b334915ba18972355a"
+    "revision": "ee2016b1ab061367dd12f7fac99fa4cb"
   },
   {
     "url": "static/d/520/path---offline-plugin-app-shell-fallback-a-30-c5a-NZuapzHg3X9TaN1iIixfv1W23E.json"
